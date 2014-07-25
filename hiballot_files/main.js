cfae20b88f04b93e41226fbb7196c19be1d02d15
Jason Axelson <jason.axelson@gmail.com>
2014-07-24 18:33:09 -1000

    Use reg-no to load data

--- hiballot_files/main.js
+++ hiballot_files/main.js
@@ -10,10 +10,11 @@ $(document).ready(function() {
     e.preventDefault();
     var $this = $(this);
     var $candidateModal = $('#candidateModal');
-    var candidate_name = $this.data('name');
+    var reg_no = $this.data('reg-no');
     var candidate_ballot_name = $this.text().replace(/\([DRLGIN]\)/, "").trim();
     var url = "http://services2.arcgis.com/tuFQUQg1xd48W6M5/arcgis/rest/services/powerballot_test4/FeatureServer/2/query"
-    var params = { where: "Ballot_name='" + candidate_ballot_name + "'", outFields: '*', f: 'pjson' }
+    //var params = { where: "Ballot_name='" + candidate_ballot_name + "'", outFields: '*', f: 'pjson' };
+    var params = { where: "CC_Reg_No='" + reg_no + "'", outFields: '*', f: 'pjson' };
     $.get(url, params, function(data) {
       console.log("got data " + data);
       // TODO: Can do error checking here? For empty-ish data?
@@ -30,7 +31,11 @@ $(document).ready(function() {
 });
 
 function parse_server_response(json_str) {
-  var data = JSON.parse(json_str).features[0].attributes;
+  var features = JSON.parse(json_str).features;
+  if (features.length == 0) {
+    throw "Invalid response - Unable to load candidate";
+  }
+  var data = features[0].attributes;
   // name -> name_party or ballot_name
   // party -> Party
   // incumbent -> incumbent_text
