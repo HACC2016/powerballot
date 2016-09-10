3802f2dbe94b068f09033f879b4ff78109bdce51
Jason Axelson <jason.axelson@gmail.com>
2016-09-10 10:14:27 -1000

    Separate getCandidates from getPowerballot
    We probably won't be using getPowerballot because we will use the DB

--- services/arc_gis.js
+++ services/arc_gis.js
@@ -1,8 +1,26 @@
 import {ReactServerAgent} from 'react-server';
 
-export function getCandidates() {
+export function getPowerBallot() {
   // HI2016G_PowerBallot_HACC
   const baseUrl = 'http://services2.arcgis.com/tuFQUQg1xd48W6M5/ArcGIS/rest/services/HACC_HI2016G_PowerBallot/FeatureServer/0/query'
+  console.log('service!')
+
+  const result = ReactServerAgent.get(baseUrl)
+  .query({
+    where: "1=1",
+    outFields: '*',
+    f: 'pjson',
+  })
+
+  return result.then((data) => {
+    return JSON.parse(data.text).features
+  })
+}
+
+export function getCandidates() {
+  // HI2016G_PowerBallot_HACC
+  const baseUrl = 'http://services2.arcgis.com/tuFQUQg1xd48W6M5/ArcGIS/rest/services/HACC_HI2016G_Candidates/FeatureServer/5/query'
+  // const baseUrl = 'http://services2.arcgis.com/tuFQUQg1xd48W6M5/ArcGIS/rest/services/HACC_HI2016G_PowerBallot/FeatureServer/0/query'
   console.log('service!')
 
   const result = ReactServerAgent.get(baseUrl)
