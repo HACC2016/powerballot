09b590f22b6f0a42f9ddad2b8e5f0af7203cbb41
Jason Axelson <jason.axelson@gmail.com>
2016-09-15 21:28:04 -1000

    Update candidate details styling/organization

--- src/components/CandidateDetails.jsx
+++ src/components/CandidateDetails.jsx
@@ -30,24 +30,35 @@ export default class CandidateDetails extends React.Component {
       <div>
         <div className={styles['candidate-name']}>{candidate.Candidate_Name} ({candidate.Candidate_Party})</div>
 
-        <div className={styles['section']}>
-          <div className={styles['general']}>
-            Seeking Office<br></br>
-            Party Affiliations<br></br>
-            Occupation<br></br>
+        <div className={styles['content']}>
+          <div>
+            <div>Seeking Office</div>
+            <div>Party Affiliations</div>
+            <div>Occupation</div>
           </div>
-          <div className={styles['separator-1']}></div>
-          <div className={styles['candidate-comittee']}>
-            <span className={styles['section-title']}>Candidate Committee</span><br></br>
-            Name<br></br>
-            Chair<br></br>
-            Treasurer<br></br>
+
+          <div className={styles['separator']}></div>
+
+          <div className={styles['section']}>
+            <div className={styles['column']}>
+              <div className={styles['section-title']}>
+                Candidate Committee
+              </div>
+              <div>Name</div>
+              <div>Chair</div>
+              <div>Treasurer</div>
+            </div>
+            <CandidateLinks className={styles['column']} title="Campaign Finance Links" links={campaignLinks} />
           </div>
-          <div className={styles['separator-2']}></div>
-          <CandidateLinks className={styles['finance-links']} title="Campaign Finance Links" links={campaignLinks} />
-          <CandidateLinks className={styles['candidate-links']} title="Candidate Links" links={candidateLinks} />
-          <CandidateLinks className={styles['other-links']} title="Other Links" links={otherLinks} />
-          <div className={styles['separator-3']}></div>
+
+          <div className={styles['separator']}></div>
+
+          <div className={styles['section']}>
+            <CandidateLinks className={styles['column']} title="Candidate Links" links={candidateLinks} />
+            <CandidateLinks className={styles['column']} title="Other Links" links={otherLinks} />
+          </div>
+
+          <div className={styles['separator']}></div>
         </div>
       </div>
     )
