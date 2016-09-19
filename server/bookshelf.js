f48e6daa8d9e0dbe32b960ccb419ab19dc05e9b0
Jason Axelson <jason.axelson@gmail.com>
2016-09-18 15:51:30 -1000

    First attempt at production db connection

--- server/bookshelf.js
+++ server/bookshelf.js
@@ -1,19 +1,20 @@
+var productionConnection = process.env.DATABASE_URL
+var devConnection = {
+  host     : '127.0.0.1',
+  user     : '',
+  password : '',
+  database : 'power_ballot_development',
+  charset  : 'utf8',
+}
+
+var connection = process.env.NODE_ENV === 'production' ? productionConnection : devConnection
+
 var knex = require('knex')({
   client: 'pg',
-  connection: {
-    host     : '127.0.0.1',
-    user     : '',
-    password : '',
-    database : 'power_ballot_development',
-    charset  : 'utf8',
-  },
+  connection,
   migrations: {
     tableName: 'migrations',
   },
-});
+})
 
-module.exports = require('bookshelf')(knex);
-
-// var User = bookshelf.Model.extend({
-//   tableName: 'users',
-// });
+module.exports = require('bookshelf')(knex)
