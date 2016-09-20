65a4741c06bb2c24e78d8176a8b5d2aa922efc0f
Jason Axelson <jason.axelson@gmail.com>
2016-09-19 20:26:57 -1000

    Remove unused imports

--- src/components/AdminCandidatePage.jsx
+++ src/components/AdminCandidatePage.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 
 import AdminCandidate from 'src/components/AdminCandidate'
 
--- src/components/StatewideBallotPage.jsx
+++ src/components/StatewideBallotPage.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 
 import { getStatewideBallot } from 'src/services/api'
 
