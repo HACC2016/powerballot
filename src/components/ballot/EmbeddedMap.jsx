0f27686d6a07c392f14ca451ef5d044b9120c34d
Jason Axelson <jason.axelson@gmail.com>
2016-09-22 08:33:50 -1000

    use query-string to build embedded map url

--- src/components/ballot/EmbeddedMap.jsx
+++ src/components/ballot/EmbeddedMap.jsx
@@ -1,10 +1,30 @@
 import React, { PropTypes } from 'react'
+import queryString from 'query-string'
 
 import styles from './embedded-map.scss'
 
 export default class EmbeddedMap extends React.Component {
   render () {
     const { latitude, longitude } = this.props
+
+    const baseUrl = '//civic-celerator.maps.arcgis.com/apps/Embed/index.html'
+    let params = {
+      webmap: 'aff51516c0ed40048482165081d2b216',
+      zoom: true,
+      scale: true,
+      search: true,
+      disable_scroll: true,
+      theme: 'light',
+    }
+    if (latitude && longitude) {
+      params = {
+        ...params,
+        center: `${latitude},${longitude}`,
+        level: 16,
+      }
+    }
+
+    const url = `${baseUrl}?${queryString.stringify(params)}`
 
     return (
       <div className={styles['container']}>
@@ -15,7 +35,7 @@ export default class EmbeddedMap extends React.Component {
           scrolling="no"
           marginHeight="0"
           marginWidth="0"
-          title="HACC Power Ballot" src={`//civic-celerator.maps.arcgis.com/apps/Embed/index.html?webmap=aff51516c0ed40048482165081d2b216&amp;center=${latitude},${longitude},&amp;level=18&amp;zoom=true&amp;scale=true&amp;search=true&amp;disable_scroll=true&amp;theme=light`} />
+          title="HACC Power Ballot" src={url} />
       </div>
     )
   }
@@ -24,8 +44,4 @@ export default class EmbeddedMap extends React.Component {
 EmbeddedMap.propTypes = {
   latitude: PropTypes.number,
   longitude: PropTypes.number,
-}
-EmbeddedMap.defaultProps = {
-  latitude: -127.864,
-  longitude: 21.306,
 }
