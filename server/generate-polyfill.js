716e7f9024ffe6a9824febb1a645f1e181d6e525
Jason Axelson <jason.axelson@gmail.com>
2016-09-23 22:22:48 -1000

    Add promise polyfill

--- server/generate-polyfill.js
+++ server/generate-polyfill.js
@@ -4,6 +4,8 @@ var Polyfiller = require('polyfiller')
 
 // Polyfiller setup
 var polyfiller = new Polyfiller
-var list = polyfiller.find(['String.prototype.startsWith'])
+var ieList = polyfiller.find(['String.prototype.startsWith', 'Promise'])
+var promiseList = polyfiller.find(['Promise'])
 
-fs.writeFile('./src/assets/js/polyfills.js', polyfiller.pack(list))
+fs.writeFile('./src/assets/js/polyfill-ie.js', polyfiller.pack(ieList))
+fs.writeFile('./src/assets/js/polyfill-promise.js', polyfiller.pack(promiseList))
